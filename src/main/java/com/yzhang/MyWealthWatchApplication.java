e6aab3a9c87532980ccb09459eb3ee140ed802f3
NEXUS\zha202 <yang.zhang@data61.csiro.au>
2017-06-20 16:20:21 +1000

    build from docker-compose; configured logging to log to files; write logging message on application started;

--- src/main/java/com/yzhang/MyWealthWatchApplication.java
+++ src/main/java/com/yzhang/MyWealthWatchApplication.java
@@ -1,12 +1,23 @@
 package com.yzhang;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class MyWealthWatchApplication {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	public static void main(String[] args) {
 		SpringApplication.run(MyWealthWatchApplication.class, args);
+	}
+
+	@PostConstruct
+	void started() {
+		this.logger.info("Application started");
 	}
 }
